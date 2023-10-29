17c617ae5938875b39c63cddccc4fe0954bbffb0
MovieaT33 <132286086+MovieaT33@users.noreply.github.com>
2023-10-29 15:30:15 +0200

    Update get_db.py

--- src/utils/api/v0/db/get_db.py
+++ src/utils/api/v0/db/get_db.py
@@ -2,7 +2,7 @@ import requests
 
 
 def get_db(domen: str, data: dict[str, str], debug: bool = True) -> str:
-    response = requests.get(f"{domen}/api/v0/get_db", params=data)
+    response = requests.get(f"{domen}/api/v0/get_file", params=data)
     if debug:
         print("INFO:", response.status_code)
     return response.text
