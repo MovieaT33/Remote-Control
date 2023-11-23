528155da74c7b7862890f934bc884e33cca6acdd
Nikolas <132286086+MovieaT33@users.noreply.github.com>
2023-11-23 23:43:13 +0200

    Update block_data.py

--- src/utils/api/v0/db/block_data.py
+++ src/utils/api/v0/db/block_data.py
@@ -1,8 +1,8 @@
+from typing import Dict
 import requests
 
 
-def block_data(domen: str, data: dict[str, str], debug: bool = True) -> str:
-    response = requests.get(f"{domen}/api/v0/block_data", params=data)
-    if debug:
-        print("INFO:", response.status_code)
+def block_info(domen: str, params: Dict[str, str]) -> str:
+    response: requests.models.Response = requests.get(f"{domen}/api/v0/block_info", params=params)
+    print(f"[HTTP] {response.status_code}")
     return response.text
