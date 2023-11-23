811f6f4ce321c8a98f33585a2e3f2e440bfd0993
Nikolas <132286086+MovieaT33@users.noreply.github.com>
2023-11-23 23:45:55 +0200

    Delete src/utils/api/v0/db/receive_message.py

--- src/utils/api/v0/db/receive_message.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-
-def receive_message(domen: str, data: dict[str, str], debug: bool = True) -> str:
-    response = requests.get(f"{domen}/api/v0/receive_message", params=data)
-    if debug:
-        print("INFO:", response.status_code)
-    return response.text
