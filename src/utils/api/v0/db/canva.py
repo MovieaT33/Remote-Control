d4fb292987a9aaddc76ee094f27cfcd33c3a43d1
Nikolas <132286086+MovieaT33@users.noreply.github.com>
2023-11-23 23:45:05 +0200

    Delete src/utils/api/v0/db/canva.py

--- src/utils/api/v0/db/canva.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-
-def canva(domen: str, data: dict[str, str], debug: bool = True) -> str:
-    response = requests.get(f"{domen}/canva", params=data)
-    if debug:
-        print("INFO:", response.status_code)
-    return response.text
