8714efb7b6b94305544893893647c2b4c286cf56
Nikolas <132286086+MovieaT33@users.noreply.github.com>
2023-12-03 22:04:25 +0200

    Update send_msg.py

--- src/utils/api/v0/db/send_msg.py
+++ src/utils/api/v0/db/send_msg.py
@@ -2,7 +2,7 @@ from typing import Dict
 import requests
 
 
-def send_msg: str, params: Dict[str, str]) -> str:
+def send_msg(domen: str, params: Dict[str, str]) -> str:
     response: requests.models.Response = requests.get(f"{domen}/api/v0/send_msg", params=params)
     print(f"[HTTP] {response.status_code}")
     return response.text
